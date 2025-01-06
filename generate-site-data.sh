a61045d2fead3bbf0d94891266d0bbdfc6f82332
4Ply <gamer.4ply@gmail.com>
2025-01-06 06:26:25 +0200

    Fix site path in site generation script

--- generate-site-data.sh
+++ generate-site-data.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-./join-stats.sh | jq -f clean-data-for-site.jq >./site/data/stats.json
-echo "Data written to ./site/data/stats.json"
+./join-stats.sh | jq -f clean-data-for-site.jq >./docs/data/stats.json
+echo "Data written to ./docs/data/stats.json"

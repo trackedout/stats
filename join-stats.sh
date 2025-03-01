0312fff0055c56a9afdd905cb3c1f8ca4c726298
4Ply <gamer.4ply@gmail.com>
2025-03-01 16:58:13 +0200

    Generate Phase 6 stats

--- join-stats.sh
+++ join-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jq -s -f stats.jq <(echo '[1, 2, 3, 4, 5]') \
+jq -s -f stats.jq <(echo '[1, 2, 3, 4, 5, 6]') \
     ./output/compShardsTradeLog.json \
     ./output/compShardsAllPlayers.json \
     ./output/compEmbersAllPlayers.json \
@@ -8,4 +8,5 @@ jq -s -f stats.jq <(echo '[1, 2, 3, 4, 5]') \
     ./output/playerStatsPhase2.json \
     ./output/playerStatsPhase3.json \
     ./output/playerStatsPhase4.json \
-    ./output/playerStatsPhase5.json
+    ./output/playerStatsPhase5.json \
+    ./output/playerStatsPhase6.json
